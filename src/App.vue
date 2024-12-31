a2d25d9933233a8a2e4020f63e8d38c7e5351b72
chenjianwei01 <chenjianwei01@baidu.com>
2024-12-31 09:31:24 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,28 @@
 
 <template>
   <div>
-    Ref: {{ obj }}
+    <div>
+      shallowReactive: {{ obj }}
+      <br/>
+      reactive: {{ obj2 }}
+    </div>
+    <button @click="add">添加</button>
   </div>
   <hr/>
-  <div>
-    shallowRef: {{ obj2 }}
-  </div>
-  <hr/>
-  <div>
-    customRef: {{ myRef }}
-  </div>
-
-  <hr/>
-  <div ref="dom"> 我是dom</div>
-
-  <button @click="change">修改</button>
 </template>
 <script  setup lang="ts">
-import { ref, isRef, shallowRef, triggerRef, customRef} from 'vue';
-import type {Ref} from 'vue';
-const dom = ref();
-
-
-function MyRef<T>(value: T) {
-  let timer = null;
-  return customRef((track, trigger) => {
-    return {
-      get() {
-        track();
-        return value;
-      },
-      set(newValue) {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-          console.log('触发了！');
-          value = newValue;
-          timer = null;
-          trigger();
-        }, 500);
-      }
-    }
-  });
-
-}
-
-const myRef = MyRef<string>('customRef 大伟');
-// ref 深层次比较：  shallowRef qian层比较
-type P = {
-  name: string;
-}
-const obj: Ref<P> =  ref({name: '大伟'});
-const obj2 =  shallowRef({name: '大伟2'});
-const change = () => {
-  // obj.value.name = '大伟1111';
-  // obj2.value.name = '大伟2222';
-  // triggerRef(obj2);
-  myRef.value = 'customRef 大伟11111';
+import { ref, reactive, readonly, shallowReactive} from 'vue';
+// ref .value 
+let obj2 = shallowReactive( {name: '前端'})
+let obj = shallowReactive({foo: {
+  bar: {
+    name: '前端'}
+} })
+const add = () => {
+  obj2.name = '后端'
+  obj.foo.bar.name = '后端'
   console.log(obj);
 }
-
 </script>
 <style scoped>
 </style>
