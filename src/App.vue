2ffd0b393f75f49d0531a014832a3ea6f3069539
chenjianwei01 <chenjianwei01@baidu.com>
2025-01-06 09:54:40 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,34 @@
 <template>
   <div>
     <div>
-      shallowReactive: {{ obj }}
+      1: <input v-model="msg.foo.bar.name" type="text" />
       <br/>
-      reactive: {{ obj2 }}
+      2: <input v-model="msg.foo.bar.age" type="text" />
     </div>
-    <button @click="add">添加</button>
   </div>
   <hr/>
 </template>
 <script  setup lang="ts">
-import { ref, reactive, readonly, shallowReactive} from 'vue';
-// ref .value 
-let obj2 = shallowReactive( {name: '前端'})
-let obj = shallowReactive({foo: {
-  bar: {
-    name: '前端'}
-} })
-const add = () => {
-  obj2.name = '后端'
-  obj.foo.bar.name = '后端'
-  console.log(obj);
-}
+import { ref, reactive, watch} from 'vue';
+
+let msg = reactive({
+  foo: {
+    bar: {
+      name: '大伟',
+      age: 18
+
+    }
+  }
+});
+watch(()=> msg.foo.bar.name, (newVal, oldVal) => {
+  console.log( newVal, oldVal);
+}, {
+ //  deep: true
+ // immediate: true, // 立即执行一次
+ once: true, // 只执行一次
+ // flush: 'sync' // 同步执行   pre 组件更新之前执行 post  组件更新之后执行
+}) 
+
 </script>
 <style scoped>
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    server: {
+        port: 8080,
+    },
+    plugins: [vue()],
+});
