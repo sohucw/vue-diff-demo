42abe401ce276b2d3fd9c1746cd0468c0d6ba46e
chenjianwei01 <chenjianwei01@baidu.com>
2025-01-07 07:51:24 +0600

    feat: upate

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,34 @@
 <template>
   <div>
     <div>
-      1: <input v-model="msg.foo.bar.name" type="text" />
+      1: <input id="ipt" v-model="msg1" type="text" />
       <br/>
-      2: <input v-model="msg.foo.bar.age" type="text" />
+      2: <input v-model="msg2" type="text" />
     </div>
+    <button @click="stop">停止</button>
   </div>
   <hr/>
 </template>
 <script  setup lang="ts">
-import { ref, reactive, watch} from 'vue';
+import { ref, reactive, watchEffect} from 'vue';
 
-let msg = reactive({
-  foo: {
-    bar: {
-      name: '大伟',
-      age: 18
-
-    }
-  }
-});
-watch(()=> msg.foo.bar.name, (newVal, oldVal) => {
-  console.log( newVal, oldVal);
+let msg1 = ref('大伟1');
+let msg2 = ref('大伟2');
+const stop = watchEffect((oninvalidate) => {
+  // const input = document.querySelector('#ipt');
+  console.log('msg1:', msg1.value);
+  // console.log('msg2:', msg2.value);
+  // console.log('input:', input);
+  oninvalidate(() => {
+    console.log('before');
+  })
 }, {
- //  deep: true
- // immediate: true, // 立即执行一次
- once: true, // 只执行一次
- // flush: 'sync' // 同步执行   pre 组件更新之前执行 post  组件更新之后执行
-}) 
+  flush: 'post',
+  onTrack(event) {
+    debugger;
+    console.log('onTrack:', event);
+  },
+});
 
 </script>
 <style scoped>
