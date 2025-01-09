61c65f67b8ea873ec6d0117c7da581194cfe974e
chenjianwei01 <chenjianwei01@baidu.com>
2025-01-09 09:36:22 +0600

    feat: slot

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,22 @@
 <template>
-    <div style="display: flex;" >
-      <div @click="handerSwitch(item, index)" :class="[active ===index ? 'active' : '']" class="tabs" v-for="(item, index) in data">
-        <div>{{ item.name }}</div>
-      </div>
-    </div>
-    <component :is="comId"></component>
+  <div>
+    <Dialog>
+      <template #header>
+        <div>具名插槽 header</div>
+      </template>
+      <template #default="{data, index}">
+        <div>{{data.name}}---- {{ data.age }}---- {{ index }}</div>
+      </template>
+      <template #footer>
+        <div>具名插槽 footer</div>
+      </template>
+    </Dialog>
+  </div>
 </template>
 <script setup lang="ts">
-import { reactive, ref, markRaw, shallowRef } from 'vue';
-import A from './components/A.vue';
-import B from './components/B.vue';
-import C from './components/C.vue';
-const comId = shallowRef('A');
-const active = ref(0);
-const data = reactive([
-  {
-    name: 'A组件',
-    com:'A'
-  },
-  {
-    name: 'B组件',
-    com:'B'
-  },
-  {
-    name: 'C组件',
-    com:'C'  // __skip__
-  }
-
-])
-const handerSwitch = (item, index) => {
-  comId.value = item.com;
-  console.log(comId);
-  active.value = index;
-
-}
-</script>
-<script lang="ts">
-  export default {
-    components: {
-      A,B,C
-    }
-  }
+import { reactive, ref } from 'vue';
+import Dialog from './components/Dialog/index.vue';
+const name = ref('header');
 </script>
 <style scoped>
 .tabs {
