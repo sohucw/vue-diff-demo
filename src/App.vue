2ff962c6330d600af4532e27e67a2537c8a6ed53
chenjianwei01 <chenjianwei01@baidu.com>
2025-01-10 09:42:49 +0600

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="parent">
-
-    <el-button  @click="flag = !flag">切换组件</el-button>
-    <keep-alive :max="5">
-        <A v-if="flag"></A>
-        <B v-else></B>
-      </keep-alive>
-    </div>
+    <h1>App.vue （祖先级别）</h1>
+    <label>
+      <input v-model="val" value="red" name="color" type="radio"/>
+      红色
+    </label>
+    <label>
+      <input v-model="val" value="yellow" name="color" type="radio"/>
+      黄色
+    </label>
+    <label>
+      <input v-model="val" value="black" name="color" type="radio"/>
+      黑色
+    </label>
+    <div class="box"></div>
+    <hr/>
+    <ProvideA/>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue';
-import A from './components/A.vue';
-import B from './components/B.vue';
-const flag = ref(true);
+import {ref, provide, readonly} from 'vue';
+import ProvideA from './components/ProvideA.vue';
+const val = ref('red');
+provide('color', readonly(val));
 </script>
 <style scoped>
+
+.box {
+    height: 60px;
+    width: 60px;
+    border: 1px solid #ccc;
+    background-color: v-bind(val);
+}
 </style>
---
+++ src/components/ProvideB.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>ProvideB.vue（孙子组件）</h1>
+    <div class="box"></div>
+    <div>
+        <button @click="change">修改provide的值 </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {ref, reactive, inject} from 'vue';
+import type { Ref } from 'vue';
+const color = inject<Ref<string>>('color', ref('green'));
+const change = () => {
+    color.value = 'blue';
+}
+
+</script>
+
+<style scoped>
+.box {
+    height: 60px;
+    width: 60px;
+    border: 1px solid #ccc;
+    background-color: v-bind(color);
+}
+</style>
