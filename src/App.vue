aa6da8dbae3acd94877fd8b7945a38e824d36378
dawei <chenjianwei01@baidu.com>
2025-01-14 11:04:54 +0600

    feat: model

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <Test></Test>
   <div>
-    <button @click="change">tesst</button>
-    {{ flag }}
+    <h2>我是app.vue父组件</h2>
+    <div>isShow: {{ isShow }}</div>
+    <div>textVal: {{ text }}</div>
+    <div><button @click="isShow=!isShow">开关</button></div>
+    <hr/>
+    <vModel v-model:textVal="text" v-model="isShow"></vModel>
   </div>
 </template>
 <script setup lang="ts">
-import Test from './App';
-let flag = ref(false);
-const change = () =>{
-  flag.value = !flag.value;
-}
+import vModel from './components/v-model.vue';
+const isShow = ref(true)
+const text = ref('大伟')
 </script>
 
 <style>
