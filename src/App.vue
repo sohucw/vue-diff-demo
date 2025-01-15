f4c0d8fb23b7078150f744ef9a64b16b0d069a94
dawei <chenjianwei01@baidu.com>
2025-01-15 12:01:35 +0600

    feat: 自定义指令

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
-
 <template>
-  <div class="btns">
-      <button v-has-show="'order@create'">创建</button>
-      <button v-has-show="'order@edit'">编辑</button>
-      <button v-has-show="'order@del'">删除</button>
+  <div>
+      <div >
+          <img  v-for ="item in arr " height="500"  width="360" alt="" v-lazy="item">
+      </div>
   </div>
-  </template>
-<script setup lang="ts">
-import type {Directive} from 'vue';
-// mock
-const permission = [
-  'order@create',
-  'order@del',
-];
-const vHasShow:Directive<HTMLElement, string> = (el, bindding) => {
-  console.log(el, bindding)
-  if (!permission.includes(bindding.value)) {
-    el.style.display = 'none';
-  }
+</template>
 
+<script setup lang='ts'>
+import type { Directive } from 'vue';
+
+// glob 是懒加载的方式
+// let modules = {
+//  'xxxx': ()=> import('xxx')
+// }
+// eager 静态加载
+//  import xx from 'xxx'
+let imgeList: Record<string, {default: string}> = import.meta.glob('./assets/images/*.*', {eager: true});
+let arr = Object.values(imgeList).map(v=> v.default);
+let vLazy: Directive<HTMLImageElement, string> = async (el, binding) => {
+  let url = await import('./assets/vue.svg')
+  el.src = url.default;
+  let observer = new IntersectionObserver((entries) => {
+      console.log(entries[0], el)
+      if(entries[0].intersectionRatio>0 && entries[0].isIntersecting)  {
+        setTimeout(()=> {
+          el.src = binding.value
+        }, 2000);
+        observer.unobserve(el);
+      }
+  })
+  observer.observe(el)
 }
-
 </script>
 
-<style scoped lang='less'>
-.btns {
-    button{
-        margin: 10px;
-    }
-  }
-</style>
+<style scoped lang='less'></style>
